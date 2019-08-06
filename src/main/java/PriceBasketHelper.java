62fe30b725225003a60a352457e77a7faffedd49
Chris Dickinson <chris@dickinson.ch>
2019-08-06 23:10:13 +0100

    fixed help text

--- src/main/java/PriceBasketHelper.java
+++ src/main/java/PriceBasketHelper.java
@@ -7,7 +7,7 @@ class PriceBasketHelper {
 
     static void displayHelpIfNecessary(String[] items) {
         if (isHelpNeeded(items)) {
-            System.out.printf("%nPlease enter a list of items, separated by a space, e.g. '%s soup soup bread'%n%n", PriceBasketHelper.class.getSimpleName());
+            System.out.printf("%nPlease enter a list of items, separated by a space, e.g. '%s soup soup bread'%n%n", PriceBasket.class.getSimpleName());
             System.out.printf("The available items are:%n%n");
             ProductConfigService productConfigService = ConfigServiceProducer.getProductConfigService();
             productConfigService.getPricedProducts().keySet().forEach(name -> System.out.println(" - " + name));
