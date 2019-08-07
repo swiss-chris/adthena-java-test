fb122b768c73c046526a448cd9e0443c08e44767
Chris Dickinson <chris@dickinson.ch>
2019-08-07 17:13:22 +0100

    corrected CamelCase for class name.

--- src/test/java/util/ProductFinderTest.java
+++ src/test/java/util/ProductFinderTest.java
@@ -8,7 +8,7 @@ import java.util.List;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-public class productFinderTest {
+public class ProductFinderTest {
 
     @Test
     public void findProducts() {
