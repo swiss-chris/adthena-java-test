f9a020334e68b36e9fe6799c9e236011616a41ce
Chris Dickinson <chris@dickinson.ch>
2019-08-07 17:27:05 +0100

    added extension idea

--- src/main/java/PriceBasket.java
+++ src/main/java/PriceBasket.java
@@ -3,6 +3,7 @@ import config.ConfigServiceProducer;
 /**
  * ideas for extension: <br />
  * - add error messages if user adds wrong arguments/items <br/>
+ * - allow other currencies
  * - add i18n <br />
  * - allow configuring discounts in percent and calculate discount and discount text dynamically <br />
  * - automated tests for static Main and Helper classes as well <br />
