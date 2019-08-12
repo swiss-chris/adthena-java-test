699c08f2bbb01b91b9a86b11c7b6c8ec73977bcd
Chris Dickinson <chris@dickinson.ch>
2019-08-12 17:38:58 +0100

    simplified build.gradle and added javadoc

--- src/main/java/discounts/util/ListOperations.java
+++ src/main/java/discounts/util/ListOperations.java
@@ -6,6 +6,11 @@ import java.util.Optional;
 
 public class ListOperations {
 
+    /**
+     * @return an {@link Optional} of the items remaining after removing all {@code itemsToRemove},
+     * or {@link Optional#empty()} if at least one of the {@code itemsToRemove} could not be removed from {@code allItems}. <br />
+     * If {@code itemsToRemove} is empty, returns a copy of {@code allItems}
+     */
     public static Optional<List<String>> removeAllElements(final List<String> allItems, final List<String> itemsToRemove) {
         final ArrayList<String> allItemsCopy = new ArrayList<>(allItems);
         for (final String itemToRemove : itemsToRemove) {
